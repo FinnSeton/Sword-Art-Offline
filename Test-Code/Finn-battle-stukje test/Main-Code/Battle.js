987c0cf25b1261ebc6a40d214e65a5483a369d1a
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-04-13 18:05:58 +0200

    battletje hp bars en dobble toegevoegd

--- Test-Code/Finn-battle-stukje test/Main-Code/Battle.js
+++ Test-Code/Finn-battle-stukje test/Main-Code/Battle.js
@@ -1,23 +1,26 @@
 var playerhpbar = document.getElementById("playerhpbar");
+var enemyhpbar = document.getElementById("enemyhpbar");
 
+function dobble(min, max) {
+    var dobblesteenoutput = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return dobblesteenoutput
+}
 
 function battle() {
     var playerhp = 40
-    var enemyhp = 100
+    var enemyhp = 50
 
     var div = document.createElement("div");
     div.setAttribute("id", "playerhpbar");
-    div.innerHTML = `<b>${playerhp}HP</b>`;
+    div.innerHTML = `<b>Player ${playerhp}HP</b>`;
     div.style.backgroundImage = "linear-gradient(to right, #00ff00 "+parseFloat(playerhp).toFixed(2)+"%"+", #ff0000 0%)"
     document.body.appendChild(div);
-    
-    
 
-
-
-
-
-
+    var div = document.createElement("div");
+    div.setAttribute("id", "enemyhpbar");
+    div.innerHTML = `<b>Enemy ${enemyhp}HP</b>`;
+    div.style.backgroundImage = "linear-gradient(to right, #00ff00 "+parseFloat(enemyhp).toFixed(2)+"%"+", #ff0000 0%)"
+    document.body.appendChild(div);
 }
 
 battle()
---
+++ Test-Code/Finn-battle-stukje test/Main-Code/Kaarten.js
@@ -0,0 +1,59 @@
+const swordkaarten = [
+    {
+        'name':"holysword",
+        'type':'sword_skill',
+        'description':'A skill that damages the opponent, roll a D6, if Its 4, you deal 1.5x the damage'
+    },{
+        'name':'stab',
+        'type':'sword_skill',
+        'description': 'A skill that does 1,10x damage, roll a d4, If its a 1 or a 4, it goes to 1,4x'
+    },{
+        'name': 'Darkness Blade',
+        'type':'sword_skill',
+        'description':'Darkness blade does 1,75x the damage'
+    },{
+        'name':'Vertival Strike',
+        'type':'sword_skill',
+        'description':'A skill that does 1,25x damage'
+    }
+]
+
+const archerkaarten = [
+    {
+        'name':"holysword",
+        'type':'sword_skill',
+        'description':'A skill that damages the opponent, roll a D6, if Its 4, you deal 1.5x the damage'
+    },{
+        'name':'stab',
+        'type':'sword_skill',
+        'description': 'A skill that does 1,10x damage, roll a d4, If its a 1 or a 4, it goes to 1,4x'
+    },{
+        'name': 'Darkness Blade',
+        'type':'sword_skill',
+        'description':'Darkness blade does 1,75x the damage'
+    },{
+        'name':'Vertival Strike',
+        'type':'sword_skill',
+        'description':'A skill that does 1,25x damage'
+    }
+]
+
+const magickaarten = [
+    {
+        'name':"magic1",
+        'type':'sword_skill',
+        'description':'A skill that damages the opponent, roll a D6, if Its 4, you deal 1.5x the damage'
+    },{
+        'name':'stab',
+        'type':'sword_skill',
+        'description': 'A skill that does 1,10x damage, roll a d4, If its a 1 or a 4, it goes to 1,4x'
+    },{
+        'name': 'Darkness Blade',
+        'type':'sword_skill',
+        'description':'Darkness blade does 1,75x the damage'
+    },{
+        'name':'Vertival Strike',
+        'type':'sword_skill',
+        'description':'A skill that does 1,25x damage'
+    }
+]
