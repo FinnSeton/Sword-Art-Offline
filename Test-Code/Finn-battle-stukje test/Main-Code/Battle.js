c94fe7b9dc0a14e164515e146c9c31ae7f86b9bc
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-04-13 20:46:25 +0200

    dmg responsive

--- Test-Code/Finn-battle-stukje test/Main-Code/Battle.js
+++ Test-Code/Finn-battle-stukje test/Main-Code/Battle.js
@@ -1,14 +1,19 @@
 var playerhpbar = document.getElementById("playerhpbar");
 var enemyhpbar = document.getElementById("enemyhpbar");
+var playerhp = 100
+var enemyhp = 100
 
 function dobble(min, max) {
     var dobblesteenoutput = Math.floor(Math.random() * (max - min + 1) ) + min;
     return dobblesteenoutput
 }
 
-function battle() {
-    var playerhp = 100
-    var enemyhp = 100
+function hpsysteem() {
+    var playerhpbar = document.getElementById("playerhpbar");
+    playerhpbar.parentNode.removeChild(playerhpbar);
+
+    var enemyhpbar = document.getElementById("enemyhpbar");
+    enemyhpbar.parentNode.removeChild(enemyhpbar);
 
     var div = document.createElement("div");
     div.setAttribute("id", "playerhpbar");
@@ -21,9 +26,13 @@ function battle() {
     div.innerHTML = `<b>Enemy ${enemyhp}HP</b>`;
     div.style.backgroundImage = "linear-gradient(to right, #00ff00 "+parseFloat(enemyhp).toFixed(2)+"%"+", #ff0000 0%)"
     document.body.appendChild(div);
-
-    
-
 }
 
-battle()
+var attackBtn = document.getElementById("attackBtn");
+attackBtn.addEventListener("click", function() {
+  playerhp = playerhp - 10;
+  hpsysteem()
+});
+
+
+hpsysteem()
