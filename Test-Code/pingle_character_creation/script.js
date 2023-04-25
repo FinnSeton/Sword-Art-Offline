5bd486ae1a4be72919d40910b3cdcc95440fca3c
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-04-25 13:09:46 +0200

    character update

--- Test-Code/pingle_character_creation/script.js
+++ Test-Code/pingle_character_creation/script.js
@@ -3,11 +3,39 @@ Archer.onclick = class_picker
 Mage.onclick = class_picker
 Swordsman.onclick = class_picker
 
-let class_picked = ''
+let class_picked = '';
 
-function class_picker(event){
+const submitButton = document.querySelector('input[type="submit"]');
 
-    class_picked = this.id
-    console.log(class_picked)
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault(); // prevent form submission
 
+    const nameInput = document.getElementById('fname');
+    const lengthInput = document.getElementById('lname');
+
+    const nameValue = nameInput.value;
+    const lengthValue = lengthInput.value;
+
+    const output = {
+        name: nameValue,
+        length: lengthValue
+    };
+
+    console.log(output);
+});
+
+
+function class_picker(event) {
+    document.querySelectorAll('#cards img').forEach((img) => {
+        img.classList.remove('card-selected');
+    });
+
+    this.classList.add('card-selected');
+
+    class_picked = this.id;
+    console.log(class_picked);
 }
+
+document.querySelectorAll('#cards img').forEach((img) => {
+    img.addEventListener('click', class_picker);
+});
